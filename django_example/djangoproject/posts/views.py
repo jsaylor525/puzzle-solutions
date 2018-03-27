40a3ba5fd830f78427d4c514c379a3e74297ac70
Jonathan Saylor <jsaylor525@gmail.com>
2018-03-26 23:09:29 -0500

    Converting response to load html template

--- django_example/djangoproject/posts/views.py
+++ django_example/djangoproject/posts/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from posts')
+    # return HttpResponse('Hello from posts')
+
+    return render(request, 'posts/index.html')
